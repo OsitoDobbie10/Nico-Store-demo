de9e275ad5f36f24b6da8656f4253af51b0a8d01
ositodobbie10 <caamledzeppelin@gmail.com>
2023-07-01 15:00:37 -0600

    'Cambios a whatsapp'

--- src/Custom/CustomWhatsapp.jsx
+++ src/Custom/CustomWhatsapp.jsx
@@ -12,6 +12,19 @@ const CustomWhatsapp = () => {
         const Colorproducto = objeto.color;
         const IDproducto = objeto.id;
         const telefono = 96613735;
+        const date = Date.now();
+        const formatearfecha = (fecha)=>{
+          const nuevafecha = new Date(fecha);
+          const opciones = {
+              year:'numeric',
+              month:'long',
+              day: '2-digit'
+          }
+          const final = nuevafecha.toLocaleDateString('es-ES',opciones);
+          return final
+      }
+      const fecha = formatearfecha(date);
+
         const url = `https://api.whatsapp.com/send?phone=504${telefono}&text=
         *Asunto producto en venta*%0A
         *NC Store*%0A%0A
@@ -19,12 +32,14 @@ const CustomWhatsapp = () => {
         ${nombre}%0A
         *Producto que se llevara*%0A
         ${Descripcionproducto}%0A
-        *ID del producto de Zapato*%0A
+        *ID del producto de ${producto}*%0A
         ${IDproducto}%0A
         *Talla del producto que se compra*%0A
         ${Tallaproducto}%0A
         *Color del producto escogido*%0A
-        ${Colorproducto}`;
+        ${Colorproducto}%0A 
+        *Fecha en la que se pidio el producto escogido*%0A
+        ${fecha}%0A`;
         window.open(url);
 
     }
