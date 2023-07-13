13cc819869dff58289a8fd7972a732078135dd7b
ositodobbie10 <caamledzeppelin@gmail.com>
2023-07-12 22:20:20 -0600

    'Cambios al proyecto oficial 1'

--- src/DB/Faldas.js
+++ src/DB/Faldas.js
@@ -1,87 +1,30 @@
 import Faldas1 from "../Files/Falda01.png";
 import Faldas2 from "../Files/Falda02.png";
-import Faldas3 from "../Files/Falda03.png";
-import Faldas4 from "../Files/Falda04.png";
-import Faldas5 from "../Files/Falda05.png";
-import Faldas6 from "../Files/Falda06.png";
-import Faldas7 from "../Files/Falda07.png";
-import Faldas8 from "../Files/Falda08.png";
-import Faldas9 from "../Files/Falda09.png";
+
 
 const Faldas = [
     {
         id:1,
-        Talla:'22-28',
+        Talla:'SM',
         color:'Negro',
-        precio:300,
-        descp:'Falda de cuero negra',
+        precio:280,
+        descp:'Short de cuero negro',
+        nombre: 'Short de cuero negro',
+        estado:'Nuevo',
+        Cantidad: 1,
         imagen:Faldas1
     },
     {
-        id:2,
-        Talla:'22-28',
-        color:'Blanco',
-        precio:350,
-        descp:'Falda larga floreda',
+        id:1,
+        Talla:'SM',
+        color:'Gris',
+        precio:280,
+        descp:'Falda gris cuadriculada con rosado',
+        nombre: 'Falda gris cuadriculada',
+        estado:'Seminuevo',
+        Cantidad: 1,
         imagen:Faldas2
-    },
-    {
-        id:3,
-        Talla:'22-28',
-        color:'Verde',
-        precio:250,
-        descp:'Pantalones cortos de tela corto',
-        imagen:Faldas3
-    },
-    {
-        id:4,
-        Talla:'22-28',
-        color:'Rosada',
-        precio:400,
-        descp:'Blusa color rosada',
-        imagen:Faldas4
-    },
-    {
-        id:5,
-        Talla:'22-28',
-        color:'Negro',
-        precio:400,
-        descp:'Falda corta de cuero negro',
-        imagen:Faldas5
-    },
-    {
-        id:6,
-        Talla:'22-28',
-        color:'Negra',
-        precio:450,
-        descp:'Falda de cuero negra',
-        imagen:Faldas6
-    },
-    {
-        id:7,
-        Talla:'22-28',
-        color:'Amarilla',
-        precio:300,
-        descp:'Falda de tela rayada',
-        imagen:Faldas7
-    },
-    {
-        id:8,
-        Talla:'22-28',
-        color:'Roja',
-        precio:300,
-        descp:'Falda de tela color roja de tela',
-        imagen:Faldas8
-    },
-    {
-        id:9,
-        Talla:'22-28',
-        color:'Negro',
-        precio:300,
-        descp:'Falda de tela larga negra',
-        imagen:Faldas9
     }
-
 ];
 
 export default Faldas;
